4bfbbc25e38b5037eb847d9717e2bfc4d289cfc8
mariog360 <mariog36@gmail.com>
2026-09-05 15:12:16 -0600

    Update config.js
    added
    supabase url
    anon key

--- config.js
+++ config.js
@@ -8,6 +8,6 @@
    políticas RLS del esquema exigen estar en la lista de correos.
    ===================================================================== */
 window.SOLOSE_CONFIG = {
-  url: "",
-  anonKey: ""
+  url: "https://fkjmhfixaqpuolgqudgx.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZram1oZml4YXFwdW9sZ3F1ZGd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2MjkzODgsImV4cCI6MjEwNDIwNTM4OH0.I3Dewy43bGQNC4X_OKrd65IvF4cBK2qe-zV2h3UWct8"
 };
